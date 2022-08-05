bb19cc184c605eb7444084b40bb67c6a9643d99e
Fatih <fatihcihan666@gmail.com>
2022-08-05 18:20:29 +0300

    Add project files.

---
+++ AutomapperExample/Models/CustomderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutomapperExample.Models
+{
+    public class CustomderDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  
+        public string Email { get; set; }  
+        public int Age { get; set; }
+       
+    }
+}
