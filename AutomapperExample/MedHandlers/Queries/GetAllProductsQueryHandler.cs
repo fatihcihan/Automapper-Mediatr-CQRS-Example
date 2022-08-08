6762e4c8ab9416ff734184eb560350ad73b12938
Fatih <fatihcihan666@gmail.com>
2022-08-08 12:54:01 +0300

    Query handler yapisi olusturuldu

--- AutomapperExample/MedHandlers/Queries/GetAllProductsQueryHandler.cs
+++ AutomapperExample/MedHandlers/Queries/GetAllProductsQueryHandler.cs
@@ -1,6 +1,31 @@
-﻿namespace AutomapperExample.MedHandlers.Queries
+﻿using AutoMapper;
+using AutomapperExample.MedHandlers.Queries.QueryModels.Requests;
+using AutomapperExample.MedHandlers.Queries.QueryModels.Responses;
+using AutomapperExample.Repositories;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomapperExample.MedHandlers.Queries
 {
-    public class GetAllProductsQueryHandler
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, List<GetAllProductsQueryResponse>>
     {
+        // Mediatr paketinden gelen IRequestHandler'i implement ediyoruz daha sonra request ve responselari belirtiyoruz
+        // Implement ettikten sonra handle methodunda gerekli configurasyonlari yapiyoruz
+
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public GetAllProductsQueryHandler(IMapper mapper, IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+        public Task<List<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var productList = _productRepository.GetAllProducts();
+            var mapProductList = _mapper.Map<List<GetAllProductsQueryResponse>>(productList);
+            return Task.FromResult(mapProductList);
+        }
     }
 }
--- AutomapperExample/MedHandlers/Queries/GetProductByIdQueryHandler.cs
+++ AutomapperExample/MedHandlers/Queries/GetProductByIdQueryHandler.cs
@@ -1,6 +1,28 @@
-﻿namespace AutomapperExample.MedHandlers.Queries
+﻿using AutoMapper;
+using AutomapperExample.MedHandlers.Queries.QueryModels.Requests;
+using AutomapperExample.MedHandlers.Queries.QueryModels.Responses;
+using AutomapperExample.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomapperExample.MedHandlers.Queries
 {
-    public class GetProductByIdQueryHandler
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQueryRequest, GetProductByIdQueryResponse>
     {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public GetProductByIdQueryHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public Task<GetProductByIdQueryResponse> Handle(GetProductByIdQueryRequest request, CancellationToken cancellationToken)
+        {
+            var product = _productRepository.GetProductById(request.Id); // repodaki ilgili operasyonu cagiriyoruz (product donuyor)
+            var mapProduct = _mapper.Map<GetProductByIdQueryResponse>(product); // donen entity tipini response tipine mapliyoruz
+            return Task.FromResult(mapProduct);                             // ilgili response'u controller'a donuyoruz
+        }
     }
 }
