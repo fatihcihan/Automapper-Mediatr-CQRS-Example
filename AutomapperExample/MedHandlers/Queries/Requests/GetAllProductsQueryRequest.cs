b063c86852ac27531445b3580abd9ef31bc7e7dd
Fatih <fatihcihan666@gmail.com>
2022-08-08 12:57:04 +0300

    Queryler icin request siniflari duzenlendi

--- AutomapperExample/MedHandlers/Queries/Requests/GetAllProductsQueryRequest.cs
+++ AutomapperExample/MedHandlers/Queries/Requests/GetAllProductsQueryRequest.cs
@@ -1,6 +1,11 @@
-﻿namespace AutomapperExample.MedHandlers.Queries.QueryModels.Requests
+﻿using AutomapperExample.MedHandlers.Queries.QueryModels.Responses;
+using MediatR;
+using System.Collections.Generic;
+
+namespace AutomapperExample.MedHandlers.Queries.QueryModels.Requests
 {
-    public class GetAllProductsQueryRequest
+    public class GetAllProductsQueryRequest : IRequest<List<GetAllProductsQueryResponse>>   //IRequest<DonusTipi>
     {
+        // MediatR altindan gelen irequest'ten implement edilmesi lazim ve daha sonra donus tipini (response) yaziyoruz
     }
 }
--- AutomapperExample/MedHandlers/Queries/Requests/GetProductByIdQueryRequest.cs
+++ AutomapperExample/MedHandlers/Queries/Requests/GetProductByIdQueryRequest.cs
@@ -1,6 +1,13 @@
-﻿namespace AutomapperExample.MedHandlers.Queries.QueryModels.Requests
+﻿using AutomapperExample.MedHandlers.Queries.QueryModels.Responses;
+using MediatR;
+using System;
+
+namespace AutomapperExample.MedHandlers.Queries.QueryModels.Requests
 {
-    public class GetProductByIdQueryRequest
+    public class GetProductByIdQueryRequest : IRequest<GetProductByIdQueryResponse>     //irequest<DonusTipi>
     {
+        // MediatR altindan gelen irequest'ten implement edilmesi lazim 
+        // Parametre olarak id aldigini belirtiyoruz ve response olarak geriye tek bi tane product donuyoruz
+        public Guid Id { get; set; }        // parametre olarak id aliyor
     }
 }
