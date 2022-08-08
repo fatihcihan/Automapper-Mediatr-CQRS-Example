6668b88e7bd64cc60d3e32e8ad411cf130745984
Fatih <fatihcihan666@gmail.com>
2022-08-08 12:55:56 +0300

    Repository interface ve class yapisi olusturuldu

---
+++ AutomapperExample/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using AutomapperExample.Models;
+using System;
+using System.Collections.Generic;
+
+namespace AutomapperExample.Repositories
+{
+    public interface IProductRepository
+    {
+        public Guid AddProduct(Product product);
+        public Product UpdateProduct(Product product);
+        public bool DeleteProduct(Guid id);
+        public Product GetProductById(Guid id);
+        public List<Product> GetAllProducts();
+    }
+}
---
+++ AutomapperExample/Repositories/ProductRepository.cs
@@ -0,0 +1,68 @@
+﻿using AutomapperExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomapperExample.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+
+
+        List<Product> products;
+        public ProductRepository()
+        {
+            products = new List<Product>()
+            {
+                new Product{ Id=Guid.NewGuid(), Name="Phone", Description="Nice phone", CreateDate=DateTime.UtcNow },
+                new Product{ Id=Guid.NewGuid(), Name="Laptop", Description="Nice laptop", CreateDate=DateTime.UtcNow },
+                new Product{ Id=Guid.NewGuid(), Name="Tv", Description="Nice tv", CreateDate=DateTime.UtcNow },
+            };
+        }
+        public Guid AddProduct(Product product)
+        {
+            product.Id = Guid.NewGuid();
+            product.CreateDate = DateTime.UtcNow;
+            product.Name = "desk";
+            product.Description = "Nice desk";
+            products.Add(product);
+            var addedProduct = GetProductById(product.Id);
+            return addedProduct.Id;
+        }
+
+        public bool DeleteProduct(Guid id)
+        {
+            var deletedProduct = GetProductById(id);
+            if (products == null)
+            {
+                return false;
+            }
+            products.Remove(deletedProduct);
+            return true;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public Product GetProductById(Guid id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            //var product = GetProductById(id);
+            return product;
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            var updatedProduct = GetProductById(product.Id);
+            if (updatedProduct != null)
+            {
+                updatedProduct.Name = product.Name;
+                updatedProduct.Id = product.Id;
+                updatedProduct.Description = product.Description;
+            }
+            return updatedProduct;
+        }
+    }
+}
