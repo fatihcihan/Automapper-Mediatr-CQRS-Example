a3bdc0ef9a78b1aa3c975f35db9579a895e25aa8
Fatih <fatihcihan666@gmail.com>
2022-08-08 12:52:14 +0300

    Products controller'da gerekli http istekleri ayarlandi

--- AutomapperExample/Controllers/CustomerController.cs
+++ AutomapperExample/Controllers/CustomerController.cs
@@ -17,34 +17,33 @@ namespace AutomapperExample.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet]
-        public CustomerDto GetCustomer()
-        {
-            var customer = new Customer
-            {
-                Id = 1,
-                Name = "X",
-                Address = "Aydin",
-                Age = 30,
-                Email = "fatih@fatih.com",
-                Status = 1
-            };
-            var customerDto = _mapper.Map<CustomerDto>(customer);
-            return customerDto;
-        }
+        //[HttpGet]
+        //public CustomerDto GetCustomer()
+        //{
+        //    var customer = new Customer
+        //    {
+        //        Id = 1,
+        //        Name = "X",
+        //        Address = "Aydin",
+        //        Age = 30,
+        //        Email = "fatih@fatih.com",
+        //        Status = 1
+        //    };
+        //    var customerDto = _mapper.Map<CustomerDto>(customer);
+        //    return customerDto;
+        //}
 
-        public List<CustomerDto> GetAllCustomers()
-        {
-            List<Customer> customers = new List<Customer>()
-            {
-                new Customer {Id=1, Name="X", Address="Aydin", Age=20, Email="x@gmail.com"},
-                new Customer {Id=2, Name="Y", Address="İzmir", Age=30, Email="y@gmail.com"},
-                new Customer {Id=3, Name="Z", Address="Mugla", Age=40, Email="z@gmail.com"},
-            };
+        //public List<CustomerDto> GetAllCustomers()
+        //{
+        //    List<Customer> customers = new List<Customer>()
+        //    {
+        //        new Customer {Id=1, Name="X", Address="Aydin", Age=20, Email="x@gmail.com"},
+        //        new Customer {Id=2, Name="Y", Address="İzmir", Age=30, Email="y@gmail.com"},
+        //        new Customer {Id=3, Name="Z", Address="Mugla", Age=40, Email="z@gmail.com"},
+        //    };
 
-            var data = _mapper.Map<List<CustomerDto>>(customers);
-            return data;
-
-        }
+        //    var data = _mapper.Map<List<CustomerDto>>(customers);
+        //    return data;
+        //}
     }
 }
---
+++ AutomapperExample/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using AutomapperExample.MedHandlers.Commands.Requests;
+using AutomapperExample.MedHandlers.Queries.QueryModels.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AutomapperExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Genel olaram Send methodunu kullanıyoruz
+        private readonly IMediator _mediator;
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #region Queries
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            // Send icinde ilgili request tipini veriyoruz bu request tipine gore mediator hangi handle'i cagiracagina karar veriyor.
+            return Ok(await _mediator.Send(new GetAllProductsQueryRequest()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            return Ok(await _mediator.Send(new GetProductByIdQueryRequest { Id = id }));
+        }
+
+        #endregion
+
+
+
+        #region Commands
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductCommandRequest request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await _mediator.Send(new DeleteProductCommandRequest { Id = id }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, UpdateProductCommandRequest request)
+        {
+            request.Id = id;
+            return Ok(await _mediator.Send(request));
+        }
+        #endregion
+
+    }
+}
