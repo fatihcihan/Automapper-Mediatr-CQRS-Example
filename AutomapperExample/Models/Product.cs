29cd4bb9aa28e3162f5599ef0f5ee7545267df59
Fatih <fatihcihan666@gmail.com>
2022-08-08 12:54:45 +0300

    Modeller olusturuldu

---
+++ AutomapperExample/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AutomapperExample.Models
+{
+    public class Product
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
