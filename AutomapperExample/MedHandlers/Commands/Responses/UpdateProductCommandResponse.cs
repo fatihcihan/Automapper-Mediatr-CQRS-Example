3374a5e25e7aadc33fe463591048b4c68dce6e99
Fatih <fatihcihan666@gmail.com>
2022-08-08 12:56:32 +0300

    Command icin response olusturuldu

--- AutomapperExample/MedHandlers/Commands/Responses/UpdateProductCommandResponse.cs
+++ AutomapperExample/MedHandlers/Commands/Responses/UpdateProductCommandResponse.cs
@@ -1,6 +1,12 @@
-﻿namespace AutomapperExample.MedHandlers.Commands.Responses
+﻿using System;
+
+namespace AutomapperExample.MedHandlers.Commands.Responses
 {
     public class UpdateProductCommandResponse
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
     }
 }
