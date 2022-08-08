347c4e8e946873c41675df48cd42cc63946adf3a
Fatih <fatihcihan666@gmail.com>
2022-08-08 12:57:38 +0300

    Queryler icin gerekli response yapisi olusturuldu

--- AutomapperExample/MedHandlers/Queries/Responses/GetAllProductsQueryResponse.cs
+++ AutomapperExample/MedHandlers/Queries/Responses/GetAllProductsQueryResponse.cs
@@ -1,6 +1,12 @@
-﻿namespace AutomapperExample.MedHandlers.Queries.QueryModels.Responses
+﻿using System;
+
+namespace AutomapperExample.MedHandlers.Queries.QueryModels.Responses
 {
     public class GetAllProductsQueryResponse
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
